7dcb03d5063d79b5f4111801630a0a345b137e46
madmax <madmax.wp@proton.me>
2026-09-15 10:04:12 +0300

    Add documentation configuration

---
+++ docs/source/conf.py
@@ -0,0 +1,20 @@
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath("../.."))
+sys.path.insert(0, os.path.abspath("../../src"))
+
+project = "mediawiki-abusefilter"
+author = "Max <madmax.wp@proton.me>"
+release = "0.1.0"
+version = "0.1.0"
+
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+]
+
+templates_path = ["_templates"]
+exclude_patterns = []
+
+html_theme = "sphinx_rtd_theme"
