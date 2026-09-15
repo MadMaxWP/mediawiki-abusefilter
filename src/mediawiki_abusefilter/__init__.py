0775ad7e507240e45d8476759f2be3b7cc9fce6d
madmax <madmax.wp@proton.me>
2026-09-15 10:04:12 +0300

    Add package entry point

---
+++ src/mediawiki_abusefilter/__init__.py
@@ -0,0 +1,38 @@
+from .auth import Auth
+from .client import Filters
+from .filter import Filter
+from .exceptions import (
+    AbuseFilterError,
+    FilterError,
+    FilterNotFoundError,
+    FilterPermissionError,
+    FilterSaveError,
+    FilterSyntaxError,
+    LoginError,
+    RuleEditError,
+    ValidationError,
+)
+
+
+# keep the package version in one place
+__version__ = "0.1.0"
+
+auth = Auth
+filters = Filters
+
+__all__ = [
+    "auth",
+    "filters",
+    "Auth",
+    "Filters",
+    "Filter",
+    "AbuseFilterError",
+    "LoginError",
+    "FilterError",
+    "FilterNotFoundError",
+    "FilterPermissionError",
+    "FilterSaveError",
+    "FilterSyntaxError",
+    "ValidationError",
+    "RuleEditError",
+]
